947bbb77587dc741df25f6b67ffc0eeda3cc41ef
unknown <aisyah000326@gmail.com>
2023-04-11 14:40:49 +0800

    login

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.json())
 
 app.post('/login', (req, res) => {
   console.log(req.body)
+  let result = login(
+    req.body.username,
+    req.body.password
+  )
   res.send('Login!')
 })
 
@@ -17,10 +21,72 @@ app.get('/bye', (req, res) => {
   res.send('Bye Bye UTeM!')
 })
 
+
 app.post('/register', (req, res) => {
-  res.send('Account created!')
+  let result = register(
+    req.body.username,
+    req.body.password,
+    req.body.name,
+    req.body.email
+  )
+  res.send(result)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+let dbUsers = [ 
+  { 
+      username: "aisyah", 
+      password: "13579", 
+      name: "aisyah", 
+      email: "aisyah000326@gmail.com" 
+  }, 
+  { 
+      username: "sarah", 
+      password: "12345", 
+      name: "sarah", 
+      email: "sarah@gmail.com" 
+  }, 
+  { 
+      username: "lee", 
+      password: "98765", 
+      name: "lee", 
+      email: "lee@gmail.com" 
+  } 
+] 
+
+function login (reqUsername,reqPassword){ 
+ let matchUser = dbUsers.find(                            //find 
+  user => user.username == reqUsername // x => console.log(x), => dia buat semua  
+  )    
+  if (!matchUser)return "User not found!" 
+  if(matchUser.password == reqPassword){ 
+      return matchUser 
+  }else{ 
+      return "Invalid password" 
+  } 
+
+  //for(let i=0: i <dbUsers.length: i++) 
+  //console.log(dbUsers[i]) 
+  // if (dbUsers[i].username == reqUsername) 
+
+} 
+
+function register(reqUsername,reqPassword,reqName,reqEmail){ 
+  dbUsers.push({                                                  //push 
+  username: reqUsername, 
+  password: reqPassword, 
+  name: reqName, 
+  email: reqEmail 
+  }) 
+} 
+
+//try to login 
+console.log(login ("alifah","123")) //valid 
+console.log(login("utem","password")) //user not found  
+
+register("utem","password","fkekk","fkekk@m.com") 
+
+console.log(login("utem","password"))
